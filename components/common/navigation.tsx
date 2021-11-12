ee6b1a70f21caacd514bd64bddf56e534b363664
andrejnano <nano@andrejnano.sk>
2021-11-12 17:52:22 +0800

    fix link

--- components/common/navigation.tsx
+++ components/common/navigation.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import logo from '../../public/img/skyway-logo-low.png'
+import Link from 'next/link'
 
 export const Navigation = () => {
   const navigation = {
@@ -16,20 +17,24 @@ export const Navigation = () => {
       <nav aria-label="top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="">
-            <a href="/" className="flex items-center">
-              <Image className="h-8 w-auto" height={32} width={120} src={logo} alt="Logo" />
-            </a>
+            <Link href="/" passHref>
+              <Image
+                className="h-8 w-auto cursor-pointer"
+                height={32}
+                width={120}
+                src={logo}
+                alt="Logo"
+              />
+            </Link>
           </div>
           <div className="">
             <div className="ml-10 flex items-baseline flex-col sm:flex-row ">
               {navigation.pages.map((page) => (
-                <a
-                  href={page.href}
-                  key={page.name}
-                  className="px-3 py-2 rounded-md text-sm font-medium text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                >
-                  {page.name}
-                </a>
+                <Link href={page.href} key={page.name} passHref>
+                  <a className="px-3 py-2 rounded-md text-sm font-medium text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
+                    {page.name}
+                  </a>
+                </Link>
               ))}
             </div>
           </div>
